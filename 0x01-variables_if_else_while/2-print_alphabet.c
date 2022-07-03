1912126a7d6417964485d9d8a5ece26f60496f84
Linda <lindawillis334@gmail.com>
2022-07-03 12:54:09 -0700

    djfhf

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include	<srdio.h>
+/**
+ * main	-	main function
+ * Return: 0
+ */
+int	main(void)
+{	int	ch;
+for	(ch	=	'a'; ch	<=	'z';	ch++;
+{	putchar(ch);
+}
+{	putchar('\n');
+}
+	return	(0);
+}

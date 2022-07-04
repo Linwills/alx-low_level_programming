f1ecec1fe01245b5e232357de41b8720d70481eb
Linda <lindawillis334@gmail.com>
2022-07-04 14:10:17 -0700

    C-programming excercise

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,3 @@
-#include	<srdio.h>
-/**
- * main	-	main function
- * Return: 0
- */
-int	main(void)
-{	int	ch;
-for	(ch	=	'a'; ch	<=	'z';	ch++;
-{	putchar(ch);
-}
-{	putchar('\n');
-}
-	return	(0);
-}
+#include	<stdlib.h>
+#include	<time.h>
+#include	<stdio.h>

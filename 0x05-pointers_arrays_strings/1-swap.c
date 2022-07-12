de2f52974f4e837d37392a7381e852948421f4c5
Linda <lindawillis334@gmail.com>
2022-07-12 11:27:41 -0700

    C-programming excercise with loops

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,15 @@
 #include	<main.h>
+#include	<stdio.h>
+
+/**
+ * 1-swap.c - swaps the values for a and b
+ * takes the in puts 
+ * Return : Always 0
+ */
+
+void	swap_int(int	*a,	int	*b);
+{
+	*a	=	42;
+	*b	=	98;
+	return;
+}
